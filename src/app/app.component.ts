02f7c2e641ffe8b708bdbe9a17b8909cc95f93e5
leonelfonsec <n.fonsecao@uniandes.edu.co>
2025-03-13 08:30:56 -0500

    instalacion de dependencia e inicio de la pantalla inicar sesion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,11 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { CommonModule } from '@angular/common';
+import { IniciarsesionComponent } from './iniciarsesion/iniciarsesion.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [CommonModule, RouterOutlet, IniciarsesionComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
