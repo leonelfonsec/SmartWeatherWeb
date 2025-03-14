a214b01ebd3e7d30e06d8ba664d0502834de7041
leonelfonsec <n.fonsecao@uniandes.edu.co>
2025-03-14 08:52:22 -0500

    correcciones en el merge

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,32 +1,40 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true, // Si estás usando Angular standalone components
+  imports: [CommonModule], // Asegúrate de importar CommonModule aquí
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
-configuracion() {
-  this.router.navigate(['/configuracion']);
-}
-activarSesion() {
-  this.router.navigate(['/sesiones']);
-}
-constructor(private router: Router) {}
-
-confirmLogout() {
-    const confirmed = confirm('¿Estás seguro de que deseas cerrar sesión?');
-    if (confirmed) {
-      this.router.navigate(['/iniciar-sesion']);
-    }
-  }
+  constructor(private router: Router) {}
+  menuOpen = false;
 
   toggleMenu() {
-    const sidebar = document.getElementById('sidebar');
-    const overlay = document.getElementById('overlay');
-    sidebar?.classList.toggle('active');
-    overlay?.classList.toggle('active');
+    this.menuOpen = !this.menuOpen;
   }
+
+  forecast = [
+    { name: 'Lunes', min: 11, max: 20, avg: 15 },
+    { name: 'Martes', min: 12, max: 20, avg: 16 },
+    { name: 'Miércoles', min: 11, max: 19, avg: 15 },
+    { name: 'Jueves', min: 11, max: 19, avg: 15 },
+    { name: 'Viernes', min: 11, max: 20, avg: 16 }
+  ];
+
+  alarmasactivas() {
+    this.router.navigate(['/alarmas-activas']);
+  }
+
+  configuracion() {
+    this.router.navigate(['/configuracion']);
+  }
+
+  sesiones() {
+    this.router.navigate(['/sesiones']);
+  }
+
 }
