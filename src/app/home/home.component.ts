03353eded648cb32cbf88f841bf122b793744edf
leonelfonsec <n.fonsecao@uniandes.edu.co>
2025-03-13 21:49:50 -0500

    finalizacion pantalla home

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,11 +1,25 @@
 import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-home',
-  imports: [],
+  standalone: true, // Si estás usando Angular standalone components
+  imports: [CommonModule], // Asegúrate de importar CommonModule aquí
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+  menuOpen = false;
 
+  toggleMenu() {
+    this.menuOpen = !this.menuOpen;
+  }
+
+  forecast = [
+    { name: 'Lunes', min: 11, max: 20, avg: 15 },
+    { name: 'Martes', min: 12, max: 20, avg: 16 },
+    { name: 'Miércoles', min: 11, max: 19, avg: 15 },
+    { name: 'Jueves', min: 11, max: 19, avg: 15 },
+    { name: 'Viernes', min: 11, max: 20, avg: 16 }
+  ];
 }
