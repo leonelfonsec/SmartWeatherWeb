a320b3e043b8bee2db7485090b9022e9826c7f66
Leonardo Bustamante <Diegol.bustamantep@gmail.com>
2025-03-14 00:09:50 -0500

    Se agregam pantallas faltantes y menu

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,14 @@
 import { Routes } from '@angular/router';
 import { IniciarsesionComponent } from './iniciarsesion/iniciarsesion.component';
 import { HomeComponent } from './home/home.component';
+import { SesionesActivasComponent } from './sesiones-activas/sesiones-activas.component';
+import { ConfiguracionComponent } from './configuracion/configuracion.component';
 
 export const routes: Routes = [
   { path: '', redirectTo: 'iniciar-sesion', pathMatch: 'full' }, // Redirige a la pantalla de inicio de sesión
   { path: 'iniciar-sesion', component: IniciarsesionComponent }, // Ruta de Iniciar Sesión
-  { path: 'home', component: HomeComponent } // Ruta de Home
+  { path: 'home', component: HomeComponent },
+  { path: 'sesiones', component: SesionesActivasComponent },
+  { path: 'configuracion', component: ConfiguracionComponent }
 ];
 
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-home',
@@ -7,5 +8,25 @@ import { Component } from '@angular/core';
   styleUrl: './home.component.scss'
 })
 export class HomeComponent {
+configuracion() {
+  this.router.navigate(['/configuracion']);
+}
+activarSesion() {
+  this.router.navigate(['/sesiones']);
+}
+constructor(private router: Router) {}
 
+confirmLogout() {
+    const confirmed = confirm('¿Estás seguro de que deseas cerrar sesión?');
+    if (confirmed) {
+      this.router.navigate(['/iniciar-sesion']);
+    }
+  }
+
+  toggleMenu() {
+    const sidebar = document.getElementById('sidebar');
+    const overlay = document.getElementById('overlay');
+    sidebar?.classList.toggle('active');
+    overlay?.classList.toggle('active');
+  }
 }
